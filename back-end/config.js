910ffd8e728d526f9d029030e2b2657441bbdd93
Gabriel <gabrielb102@gmail.com>
2023-02-07 16:46:36 -0800

    Changed DB URL in backend

--- back-end/config.js
+++ back-end/config.js
@@ -4,10 +4,12 @@ const PORT = 3001;
 const BCRYPT_SALT_ROUNDS = 10;
 const API_KEY = "rkqWOfeqjuCrYVDy0ASrs9nQj0vLcHfmOcaIQIq6";
 const REDIS_URI = process.env.REDIS_ENV ? process.env.REDIS_ENV : '';
-const DB_URI =
+const DEFAULT_DB_URI =
   process.env.NODE_ENV === 'test'
     ? 'postgresql:///receiptsus_test'
     : 'postgresql:///receiptsus';
+
+const DB_URI = process.env.PSQL_ENV ? process.env.PSQL_ENV : DEFAULT_DB_URI;
 
 module.exports = {
     SECRET_KEY,
