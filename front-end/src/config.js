454c1aad698b64fe95a1ea42b6b767cdf76d6a1c
Gabriel <gabrielb102@gmail.com>
2023-02-15 11:37:50 -0800

    changed web address for BackEnd to http for testing

--- front-end/src/config.js
+++ front-end/src/config.js
@@ -1,3 +1,3 @@
-const API_URL = "https://pcapi-env.eba-rfcunpnk.us-west-1.elasticbeanstalk.com:3001";
+const API_URL = "http://pcapi-env.eba-rfcunpnk.us-west-1.elasticbeanstalk.com:3001";
 
 export default API_URL;
