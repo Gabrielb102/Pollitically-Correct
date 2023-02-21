6c0b378ab3b0470bbcae387df6e4351b82d75115
Gabriel <gabrielb102@gmail.com>
2023-02-20 16:14:02 -0800

    Trying out using environment variables with AWS Amplify

--- front-end/src/config.js
+++ front-end/src/config.js
@@ -1,3 +1,3 @@
-const API_URL = "https://ssl.gburgos.us-west-1.elasticbeanstalk.com/";
+const API_URL = $BACKEND_URL || "https://ssl.gburgos.us-west-1.elasticbeanstalk.com:3001";
 
 export default API_URL;
