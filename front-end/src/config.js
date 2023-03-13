8b3bfe1e4a3de6e0c3faa0e6cc070c9a36d20768
Gabriel <gabrielb102@gmail.com>
2023-03-13 13:24:55 -0700

    Added local address so I can just switch back and forth between server addresses for interviews

--- front-end/src/config.js
+++ front-end/src/config.js
@@ -1,4 +1,4 @@
-// const API_URL = "https://gburgos.us-west-1.elasticbeanstalk.com";
 const API_URL = "https://gburgos.us-west-1.elasticbeanstalk.com";
+// const API_URL = "http://127.0.0.1:3001";
 
 export default API_URL;
